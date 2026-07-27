a2dbb670cc2cecf9cfda51c43f14c4be8c0ffa5d
Lesliechan201@gmail.com <Lesliechan201@gmail.com>
2026-07-27 18:23:31 -0400

    Fixed a bug with Retaliation causing swords to attack slower at times.

--- Source/EpicLoot/Leslie Magic Effects/MagicEffectss/Retaliation.cs
+++ Source/EpicLoot/Leslie Magic Effects/MagicEffectss/Retaliation.cs
@@ -4,6 +4,7 @@ using EpicLootLeslieAlphaTest.src.StatusEffects;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace EpicLootLeslieAlphaTest.src.MagicEffectss
 {
     public static class Retaliation
     {
+        private static readonly Stopwatch chainSw = new Stopwatch();
+        private static readonly Stopwatch attackSw = new Stopwatch();
+        private static long lastNow = 0;
         public static string AttackType;
 
         [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.BlockAttack))]
@@ -40,13 +44,14 @@ namespace EpicLootLeslieAlphaTest.src.MagicEffectss
             }
         }
 
-        [HarmonyPatch(typeof(Attack), nameof(Attack.OnAttackTrigger))]
+        [HarmonyPatch(typeof(Attack), nameof(Attack.Start))]
         public class Humanoid_Attack_Patch
         {
-            static void Prefix(Attack __instance)
+            static void Prefix(Attack __instance, Humanoid character)
             {
-                if (__instance.m_character != Player.m_localPlayer || !Player.m_localPlayer.HasActiveMagicEffect("Retaliation", out float _, 1f)) return;
+                if (character != Player.m_localPlayer || !Player.m_localPlayer.HasActiveMagicEffect("Retaliation", out float _, 1f)) return;
                 AttackType = __instance.m_attackAnimation;
+                Jotunn.Logger.LogError($" Attack Start logs {AttackType} character anim speed {Player.m_localPlayer.m_animator.speed} player attack anim{Player.m_localPlayer.m_actionAnimation}");
                 //var se = Player.m_localPlayer.GetSEMan().GetStatusEffect(SE_Retaliation.EffectName.GetStableHashCode()) as SE_Retaliation;
                 //if (!__instance.m_character.InAttack() && se.m_time >= .3f) se.ConsumeStack();
             }
