a2b6623cab2de343f7cddb801a3daf1c1c9d8fa6
jneb802 <73610029+jneb802@users.noreply.github.com>
2026-08-11 12:18:44 -0700

    Use Epic Loot reinitialize for chest ward bypass

--- Source/EpicLoot/TreasureChestWardAccessPatches.cs
+++ Source/EpicLoot/TreasureChestWardAccessPatches.cs
@@ -1,55 +1,20 @@
 using EpicLoot.Adventure;
 using HarmonyLib;
-using UnityEngine;
 
 namespace PraetorisClient.EpicLootFeature
 {
-    internal static class TreasureChestWardAccess
-    {
-        private const string TreasureMapChestBiomeKey = "TreasureMapChest.Biome";
-
-        internal static void DisableGuardStoneCheckForTreasureChest(Container container)
-        {
-            if (container == null || !container.m_checkGuardStone || !IsEpicLootTreasureChest(container))
-            {
-                return;
-            }
-
-            container.m_checkGuardStone = false;
-        }
-
-        private static bool IsEpicLootTreasureChest(Component component)
-        {
-            if (component.GetComponentInParent<TreasureMapChest>() != null)
-            {
-                return true;
-            }
-
-            ZNetView zNetView = component.GetComponent<ZNetView>();
-            if (zNetView == null || !zNetView.IsValid())
-            {
-                return false;
-            }
-
-            return !string.IsNullOrEmpty(zNetView.GetZDO().GetString(TreasureMapChestBiomeKey));
-        }
-    }
-
-    [HarmonyPatch(typeof(Container), "Awake")]
-    internal static class EpicLootTreasureChestContainerAwakePatch
-    {
-        private static void Postfix(Container __instance)
-        {
-            TreasureChestWardAccess.DisableGuardStoneCheckForTreasureChest(__instance);
-        }
-    }
-
     [HarmonyPatch(typeof(TreasureMapChest), nameof(TreasureMapChest.Reinitialize))]
     internal static class EpicLootTreasureChestReinitializePatch
     {
         private static void Postfix(TreasureMapChest __instance)
         {
-            TreasureChestWardAccess.DisableGuardStoneCheckForTreasureChest(__instance.GetComponent<Container>());
+            Container container = __instance.GetComponent<Container>();
+            if (container == null || !container.m_checkGuardStone)
+            {
+                return;
+            }
+
+            container.m_checkGuardStone = false;
         }
     }
 }
